7befaac6a4a19e661cccc8b9560f3b0b0a11bfd0
newthings0 <felixn.1162@gmail.com>
2024-12-24 11:30:48 -0800

    first commit with authentication

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,141 @@
-import Image from "next/image";
-
-export default function Home() {
+// import ListingItem from '@/components/ListingItem';
+import Link from 'next/link';
+export default async function Home() {
+  // let rentListings = null;
+  // try {
+  //   const result = await fetch(process.env.URL + '/api/listing/get', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       type: 'rent',
+  //       limit: 4,
+  //       order: 'asc',
+  //     }),
+  //     cache: 'no-store',
+  //   });
+  //   const data = await result.json();
+  //   rentListings = data;
+  // } catch (error) {
+  //   rentListings = { title: 'Failed to load listing' };
+  // }
+  // let saleListings = null;
+  // try {
+  //   const result = await fetch(process.env.URL + '/api/listing/get', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       type: 'sale',
+  //       limit: 4,
+  //       order: 'asc',
+  //     }),
+  //     cache: 'no-store',
+  //   });
+  //   const data = await result.json();
+  //   saleListings = data;
+  // } catch (error) {
+  //   saleListings = { title: 'Failed to load listing' };
+  // }
+  // let offerListings = null;
+  // try {
+  //   const result = await fetch(process.env.URL + '/api/listing/get', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       limit: 4,
+  //       order: 'asc',
+  //       offer: true,
+  //     }),
+  //     cache: 'no-store',
+  //   });
+  //   const data = await result.json();
+  //   offerListings = data;
+  // } catch (error) {
+  //   offerListings = { title: 'Failed to load listing' };
+  // }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      {/* <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-slate-500'>perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          Sahand Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Link
+          href={'/search'}
+          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Let&apos;s get started...
+        </Link>
+      </div>
+      <img
+        src='https://firebasestorage.googleapis.com/v0/b/mern-auth-1c4ae.appspot.com/o/1693307829089home%203.jpeg?alt=media&token=8dcc9a22-a8d3-4737-b27f-7c77b417a7d0'
+        className='w-full h-[550px] object-cover'
+      />
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {offerListings && offerListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent Offers
+              </h2>
+              <Link
+                className='text-sm text-blue-800 hover:underline'
+                href={'/search?offer=true'}
+              >
+                Show more listings
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {rentListings && rentListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent places for rent
+              </h2>
+              <Link
+                className='text-sm text-blue-800 hover:underline'
+                href={'/search?type=rent'}
+              >
+                Show more places for rent
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>
+                Recent places for sale
+              </h2>
+              <Link
+                className='text-sm text-blue-800 hover:underline'
+                href={'/search?type=sale'}
+              >
+                Show more places for sale
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {saleListings.map((listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div> */}
     </div>
   );
 }
