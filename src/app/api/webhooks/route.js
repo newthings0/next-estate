52d871a6bf5bae20056a05348049f68ea9c29de6
newthings0 <felixn.1162@gmail.com>
2024-12-26 05:17:39 -0800

    add mongodb and saves user from clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -146,19 +146,49 @@ export async function POST(req) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
-  const eventType = evt.type;
+  const { id } = evt?.data
+  const eventType = evt?.type;
 
-  if (evt.type=== 'user.created') {
-   console.log('user.created');
+  if (eventType === 'user.created' || eventType === 'user.updated') {
+    const { first_name, last_name, image_url, email_addresses } = evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses
+      );
+      if (user && eventType === 'user.created') {
+        try {
+          await clerkClient.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMogoId: user._id,
+            },
+          });
+        } catch (error) {
+          console.log('Error: Could not update user metadata:', error);
+        }
+      }
+    } catch (error) {
+      console.log('Error: Could not create or update user:', error);
+      return new Response('Error: Could not create or update user', {
+        status: 400,
+      });
+    }
   }
 
-  if (evt.type=== 'user.updated') {
-   console.log('user.updated');
+  if (eventType === 'user.deleted') {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log('Error: Could not delete user:', error);
+      return new Response('Error: Could not delete user', {
+        status: 400,
+      });
+    }
   }
-  if (evt.type=== 'user.deleted') {
-   console.log('user.deleted');
-  }
+
   
   return new Response('Webhook received', { status: 200 })
 }
