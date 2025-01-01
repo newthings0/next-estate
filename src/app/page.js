bf06dea6a1c734a18b242ab1bdac4abe977ee737
newthings0 <felixn.1162@gmail.com>
2025-01-01 15:40:33 -0800

    image edit home

--- src/app/page.js
+++ src/app/page.js
@@ -72,10 +72,17 @@ export default async function Home() {
         </Link>
       </div>
       <Image
+          src='/f1.jpg'
+          width={316}
+          height={190}
+          alt='home cover'
+          className='w-full h-[550px] object-cover'
+        />
+      {/* <Image
         src='https://firebasestorage.googleapis.com/v0/b/mern-auth-1c4ae.appspot.com/o/1693307829089home%203.jpeg?alt=media&token=8dcc9a22-a8d3-4737-b27f-7c77b417a7d0'
         alt='home cover'
         className='w-full h-[550px] object-cover'
-      />
+      /> */}
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
         {offerListings && offerListings.length > 0 && (
           <div className=''>
